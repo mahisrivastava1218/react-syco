067a9e6d576e322c186a0fba1f601b42dccc88a8
Mahima <priyanshisrivastava708016@gmail.com>
2023-03-25 10:51:37 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+// import export by ES6 
 function App() {
+  let name = 'hina';
   return (
+    // we can directly use js under html which is provide by jsx
+   
     <div className="App">
       <header className="App-header">
+      <h1>{name}</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
